a5e4c293d0c53cf96979bf974f4a73879127f079
esraa237 <esraafoda2000@gmail.com>
2024-04-17 00:23:16 +0200

    finish nav bar

---
+++ script.js
@@ -0,0 +1,22 @@
+const nav = document.querySelector(".nav"),
+  searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("fa-magnifying-glass", "fa-xmark");
+  }
+  searchIcon.classList.replace("fa-xmark","fa-magnifying-glass");
+});
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
